6896270eb04e5e930719748d677bbb4d9d59f480
VictorPiggy <samgookgy3@gmail.com>
2026-01-15 11:17:50 +0900

    feat: Print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,4 @@
-result = ["fizz" if i % 5 == 0 else i for i in range(16, 30)] #Implementation of fizzbuzz
+result = ["fizzbuzz" if i % 5 == 0 else i for i in range(1, 30)] #Implementation of fizzbukzz
+
 
 print(*result, sep="\n")
